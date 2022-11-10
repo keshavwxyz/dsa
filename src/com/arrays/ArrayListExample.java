1cf6658921f682a2144872483f2c60bb2de94d17
extra_caffeine <keshavbhardwaj5354@gmail.com>
2022-11-10 20:05:20 +0530

    idea

--- src/com/arrays/ArrayListExample.java
+++ src/com/arrays/ArrayListExample.java
@@ -1,5 +1,26 @@
 package com.arrays;
 
-public class ArrayListExample {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class ArrayListExample {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        ArrayList<Integer> list = new ArrayList<>(10);
+        list.add(123);
+        list.add(123);
+        list.add(234);
+        list.add(345);
+
+        System.out.println(list);
+        
+        //input
+        for (int i = 0; i < 5; i++) {
+            list.add(in.nextInt());
+        }
+        for (int i = 0; i <5; i++) {
+            System.out.println(list.get(i));
+        }
+    }
 }

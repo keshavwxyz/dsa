ebf74ad089a3a404e753c7b7af86a0180a95d0d0
extra_caffeine <keshavbhardwaj5354@gmail.com>
2023-01-24 21:29:41 +0530

    Lo bhai ye RotatedArrayWithDuplicate me first index bhi nikal liya or kya karna ha bc

--- src/com/Algo/BinarySearch/RotatedArrayWithDuplicate.java
+++ src/com/Algo/BinarySearch/RotatedArrayWithDuplicate.java
@@ -1,16 +1,32 @@
 package com.Algo.BinarySearch;
 
-public class InRotatedArray {
+public class RotatedArrayWithDuplicate {
     public static void main(String[] args) {
-        int[] arr = {8,9,12,15,17,19,20,26,37,39,40,52,62,74,85,96,1,2,3,4,5,6,7};
-        int[] arr1 = {1,2,3,4,5,6,7};
-        int[] arr2 = {2,2,2,2,9};
-        System.out.println(pivot(arr2,9));
+        int[] arr = {8,8,8,9,9,9,9,9,9,10,12,12,12,12,12,12,15,15,15,15,17,
+                19,19,19,19,19,19,20,1,2,2,2,2,2,2,3,3,3,3,3,3,3,4,5,6,7};
+        int[] arr1 = {2,2,2,2,9,2,2};
+        int target = 12;
+        System.out.println(pawd(arr,target));
+        System.out.println("The first one is at index: "+FirstIndex(arr,target));
     }
-    static int pivot(int[] arr,int target){
-        if(arr[0]<arr[arr.length-1]) return bs(arr,target,0, arr.length-1,0);
+    static int FirstIndex(int[] arr,int target){
+        int mid=pawd(arr,target);
+        while(arr[mid]==arr[mid-1]){
+            mid-=1;
+        }
+        return mid;
+    }
+    static int pawd(int[] arr,int target){
+        int start = 0,end=arr.length-1;
+        while(arr[start]==arr[end]){
+            start += 1;
+            end -= 1;
+        }
+        return pivot(arr,target,start,end);
+    }
+    static int pivot(int[] arr,int target,int start,int end){
+        if(arr[start]<arr[end]) return bs(arr,target,start, end,0);
         else{
-            int start = 0,end=arr.length-1;
             while(start<=end){
                 int mid = start +(end-start)/2;
                 if(mid<end && arr[mid]>arr[mid+1]) return bs(arr,target,0, arr.length-1,mid);
